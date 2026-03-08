a39f3f5f4a9cb02d6b18d15a1c7987b62af73e1f
THANGADURAI SELVARAJ <satselva@hpe.com>
2026-03-08 13:47:24 +0530

    Fix anon key

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://qplzaaybuyhqbjhxqsna.supabase.co",
-  supabaseAnonKey: "sb_publishable_X5oOJRynk12kt1-oCyDZfQ_yV1aIgU-"
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwbHphYXlidXlocWJqaHhxc25hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTI0MzksImV4cCI6MjA4ODQ4ODQzOX0.qFKo4gviagL2SXoOLbjtUFYBB-lvz_bxR51a-TYmobw"
 };
